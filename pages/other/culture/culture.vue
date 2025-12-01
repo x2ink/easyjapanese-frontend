1351d5cb02bf5b1c5462cdfac648010d0a3db5dd
yangchenglin <97400220@qq.com>
2025-12-01 23:16:31 +0800

    新增文化常识

---
+++ pages/other/culture/culture.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from "vue"
+	import {
+		onLoad
+	} from "@dcloudio/uni-app"
+</script>
+
+<style lang="scss" scoped>
+	       
+</style>
